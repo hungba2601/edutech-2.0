df8f630adfb2ce26cfd53616142a4c9cf3ed076b
hungba2601 <hungba2601@gmail.com>
2026-08-06 12:09:27 +0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1298,7 +1298,7 @@ const App: React.FC = () => {
         isOpen={isPensionCalculator154ModalOpen}
         onClose={() => setIsPensionCalculator154ModalOpen(false)}
         onLogin={() => handleLoginAction('pension-calculator-154')}
-        videoUrl="https://youtu.be/V-8Z-40VND8"
+        videoUrl="https://youtu.be/JFNpd4lKs_A"
       />
 
       <NotificationModal
